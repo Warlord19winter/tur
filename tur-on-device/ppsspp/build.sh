7946adc686b4f9d6d70ab85d20084d99f213ee36
Warlord19winter <76887348+Warlord19winter@users.noreply.github.com>
2026-08-20 11:23:43 -0500

    Add files via upload

--- tur-on-device/ppsspp/build.sh
+++ tur-on-device/ppsspp/build.sh
@@ -3,13 +3,11 @@ TERMUX_PKG_DESCRIPTION="A PSP emulator"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="LICENSE.TXT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="1.19.3"
+TERMUX_PKG_VERSION="1.20.4"
 TERMUX_PKG_SRCURL=git+https://github.com/hrydgard/ppsspp
 TERMUX_PKG_GIT_BRANCH="v${TERMUX_PKG_VERSION}"
 TERMUX_PKG_DEPENDS="ffmpeg, libc++, libpng, libsnappy, libzip, sdl3, sdl3-ttf, zlib"
 TERMUX_PKG_BUILD_DEPENDS="vulkan-headers"
-TERMUX_PKG_DEPENDS="libcurl, libpng, miniupnpc, zlib, libzip, glew, libsnappy, ffmpeg, libcpufeatures, rapidjson, sdl2, sdl2-ttf, fontconfig"
-TERMUX_PKG_BUILD_DEPENDS="mesa-dev, libglvnd-dev, vulkan-headers, rapidjson, spirv-headers, spirv-tools"
 
 # USE_SYSTEM_FFMPEG: the bundled ffmpeg archives are glibc builds and
 #   won't link against Bionic (undefined __isoc99_sscanf, fcntl64).
